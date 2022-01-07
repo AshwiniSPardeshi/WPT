7cfa18e83259cf36dc7ab5d770335124f3596544
Ashwini <pardeshia1998@gmail.com>
2022-01-07 11:47:22 +0530

    Connection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function App(){
-  return (<div>
-    <h1>Hellooo</h1>
-    <h1 >Good Morning... &#10084; &#10087;</h1>
-    <div>Have a nice day!</div>
-  </div> );
+export default function App() {
+  return (
+    <div>
+      <div>
+        <MyComp />
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function MyComp() {
+  const [username, setUsern] = useState("");
+  const [password, setPass] = useState("");
+  const [list, setList] = useState([]);
+
+  const userVal = (e) => {
+    setUsern(e.target.value);
+  };
+
+  const passVal = (e) => {
+    setPass(e.target.value);
+  };
+
+  const addData = async () => {
+    // alert("Done!!");
+    let user = { username: username, password: password };
+    const url = "http://localhost:4000/adduser1";
+    await axios.post(url, user);
+    setList([user, ...list]);
+    setPass("");
+    setUsern("");
+  };
+
+  const selectData = async () => {
+    const url = "http://localhost:4000/user1";
+    const result = await axios.get(url);
+    const newList = result.data;
+    setList(newList);
+    console.log(newList);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={username}
+          placeholder="Enter Username"
+          onChange={userVal}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={password}
+          placeholder="Enter Password"
+          onChange={passVal}
+        />
+      </div>
+      <div>
+        <input type="button" value="Register" onClick={addData} />
+      </div>
+      <div>
+        <input type="button" value="GetData" onClick={selectData} />
+      </div>
+      {list.map((item, index) => (
+        <div id="index">
+          {item.username} {item.password}
+        </div>
+      ))}
+    </div>
+  );
+}
